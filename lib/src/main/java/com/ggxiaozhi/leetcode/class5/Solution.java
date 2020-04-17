8993ef013e8e770800d9124d2e66688c937d5d59
Administrator <guzhigang@qinglianyun.com>
2020-04-17 19:22:09 +0800

    6-2

--- lib/src/main/java/com/ggxiaozhi/leetcode/class5/Solution.java
+++ lib/src/main/java/com/ggxiaozhi/leetcode/class5/Solution.java
@@ -1,6 +1,7 @@
 package com.ggxiaozhi.leetcode.class5;
 
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Create by ggxz
@@ -400,12 +401,240 @@ public class Solution {
         return newHead;
     }
 
-    public static void main(String[] args) {
+
+    /**
+     * 143. 重排链表
+     * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+     * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+     * <p>
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+     * <p>
+     * 示例 2:
+     * <p>
+     * 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+     * <p>
+     * 思想就是 找到中间节点 然后 断链 将后面的翻转 然后将两个链表依次向后链接
+     */
+    public static void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null)
+            return;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode head2 = slow.next;
+        slow.next = null;
+
+        head2 = reverseList(head2);
+
         /**
-         * 指针测试
+         *
+         * 1. 先取出 head2的下一个节点temp
+         * 2. head2.next=head.next
+         * 3. head.next=head2
+         * 4. head2=temp
+         *
+         * 这个难点在于指针的指向
+         * 我们要保证 head节点的next不会丢失
+         * 我们可以用temp存head2的下个一节点  head2的节点不会丢失了
+         * 然后让head2=temp 相当于head2向后移动
+         *
+         * temp=head2.next head2不会丢失
+         * 然后head2.next=head.next 保证head的下一个节点不会丢失
+         *
+         *
          */
-        ListNode listNode = rotateRight(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null))))), 2);
-        System.out.println(listNode);
+        while (head2 != null) {
+
+            ListNode temp = head2.next;
+            head2.next = head.next;
+            head.next = head2;
+            head = head2.next;
+            head2 = temp;
+        }
+    }
+
+    /**
+     * 143. 重排链表 递归
+     * https://leetcode-cn.com/problems/reorder-list/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-34/
+     * 思想是
+     * 先处理中间 然后中间处理好了 再将外面的head 和tail 连接  递归过程
+     * 1 2 3 4 5
+     * 开始：
+     * 1 2 3 4 5
+     * 1 4 2 3 5
+     * 1 5 4 2 3
+     * 这里的关键是每次递归 len-1
+     */
+    public static void reorderList2(ListNode head) {
+
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
+        }
+        int len = 0;
+        ListNode h = head;
+        //求出节点数
+        while (h != null) {
+            len++;
+            h = h.next;
+        }
+
+        reorderListHelper(head, len);
+    }
+
+    private static ListNode reorderListHelper(ListNode head, int len) {
+        if (len == 1) {
+            ListNode outTail = head.next;
+            head.next = null;
+            return outTail;
+        }
+        if (len == 2) {
+            ListNode outTail = head.next.next;
+            head.next.next = null;
+            return outTail;
+        }
+        //得到对应的尾节点，并且将头结点和尾节点之间的链表通过递归处理
+        ListNode tail = reorderListHelper(head.next, len - 2);
+        ListNode subHead = head.next;//中间链表的头结点
+        head.next = tail;
+        ListNode outTail = tail.next;  //上一层 head 对应的 tail
+        tail.next = subHead;
+        return outTail;
+    }
+
+    /**
+     * 234. 回文链表
+     * <p>
+     * 请判断一个链表是否为回文链表。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 1->2
+     * 输出: false
+     * <p>
+     * 示例 2:
+     * <p>
+     * 输入: 1->2->2->1
+     * 输出: true
+     * <p>
+     * 递归练习 根据143. 重排链表 递归思想练习
+     */
+
+    static ListNode front = null;
+
+    public static boolean isPalindrome(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        front = head;
+        return recursive(head);
+
+    }
+
+    private static boolean recursive(ListNode head) {
+        if (head.next == null) {
+            boolean b = front.val == head.val;
+            front = front.next;
+            return b;
+        }
+        if (!recursive(head.next)) {
+            return false;
+        } else {
+            boolean b = front.val == head.val;
+            front = front.next;
+
+            return b;
+        }
+    }
+
+    /**
+     * 150. 逆波兰表达式求值
+     * <p>
+     * 根据逆波兰表示法，求表达式的值。
+     * <p>
+     * 有效的运算符包括 +, -, *, / 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+     * <p>
+     * 说明：
+     * <p>
+     * 整数除法只保留整数部分。
+     * 给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入: ["2", "1", "+", "3", "*"]
+     * 输出: 9
+     * 解释: ((2 + 1) * 3) = 9
+     * <p>
+     * 示例 2：
+     * <p>
+     * 输入: ["4", "13", "5", "/", "+"]
+     * 输出: 6
+     * 解释: (4 + (13 / 5)) = 6
+     * <p>
+     * 示例 3：
+     * <p>
+     * 输入: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+     * 输出: 22
+     * 解释:
+     * ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+     * = ((10 * (6 / (12 * -11))) + 17) + 5
+     * = ((10 * (6 / -132)) + 17) + 5
+     * = ((10 * 0) + 17) + 5
+     * = (0 + 17) + 5
+     * = 17 + 5
+     * = 22
+     */
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+        int p1 = 0;
+        int p2 = 0;
+        for (int i = 0; i < tokens.length; i++) {
+            switch (tokens[i]) {
+                case "+":
+                    p2 = stack.pop();
+                    p1 = stack.pop();
+                    stack.push(p1 + p2);
+                    break;
+                case "-":
+                    p2 = stack.pop();
+                    p1 = stack.pop();
+                    stack.push(p1 - p2);
+                    break;
+                case "*":
+                    p2 = stack.pop();
+                    p1 = stack.pop();
+                    stack.push(p1 * p2);
+                    break;
+                case "/":
+                    p2 = stack.pop();
+                    p1 = stack.pop();
+                    stack.push(p1 / p2);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(tokens[i]));
+                    break;
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+
+        boolean b = isPalindrome(new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(1, null)))));
+        System.out.println(b);
 //
 //        ListNode node = new ListNode(1, new ListNode(2, null));
 //        ListNode temp = node.next;
@@ -413,7 +642,6 @@ public class Solution {
 //        System.out.println(temp.val);
 //        System.out.println(node.next.val);
 
-        System.out.println(getNodeSize(listNode));
 
     }
 }
